24a4a4fc79ec8a9b15de8b2b281447c1f1a44bf5
Rakoth <70511638+rakoth-gri@users.noreply.github.com>
2020-10-02 18:34:53 +0300

    3state_adaptive_slider

---
+++ src/JS/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    $('.slider__content__wrapper').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 1000,
+        lazyLoad: 'ondemand',
+        prevArrow: '<button type="button" class="slick-prev"> <img src="icons/prev.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"> <img src="icons/next.png"></button>'
+    });
+
+
+
+
+
+
+
+
+
+});
