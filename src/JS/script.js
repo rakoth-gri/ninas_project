a65c87e79e629c4ee0d232561c5eb8b8865ad6c1
Rakoth <70511638+rakoth-gri@users.noreply.github.com>
2020-10-04 18:10:11 +0300

    ordering_modal_adaptive

--- src/JS/script.js
+++ src/JS/script.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
         slidesToShow: 1,
         slidesToScroll: 1,
         speed: 600,
-        autoplay: true,
+        // autoplay: true,
         lazyLoad: 'ondemand',
         prevArrow: '<button type="button" class="slick-prev"> <span class="icon-arrow-left2 features"></span> </button>',
         nextArrow: '<button type="button" class="slick-next"> <span class="icon-arrow-right2 features"></span> </button>'
